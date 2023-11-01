5c3575caac7553785d8737825afb684b8c00be01
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-11-01 13:48:27 -0500

    upgrade

--- src/services/cervantes/almuerzos.services.js
+++ src/services/cervantes/almuerzos.services.js
@@ -6,7 +6,7 @@ class AlmuerzosService {
     static async getLunchInicial(school_id) {
         try {
             const result = await Clients.findAll({
-                where: { sectionId: 5, schoolId: school_id, statusLunch: false },
+                where: { sectionId: 5, schoolId: school_id, serviceId:[16,30,31,34,42,45], statusLunch: false },
                 order: [
                     ['lastName', 'ASC'],
                 ],
@@ -28,7 +28,7 @@ class AlmuerzosService {
     static async getLunchPrimaria(school_id) {
         try {
             const result = await Clients.findAll({
-                where: { sectionId:[6], schoolId: school_id, statusLunch: false },
+                where: { sectionId:2, schoolId: school_id, serviceId:[16,30,31,34,42,45], statusLunch: false },
                 order: [
                     ['lastName', 'ASC'],
                 ],
@@ -50,7 +50,7 @@ class AlmuerzosService {
     static async getLunchSecundaria(school_id) {
         try {
             const result = await Clients.findAll({
-                where: { sectionId: 7, schoolId: school_id, statusLunch: false },
+                where: { sectionId: 6, schoolId: school_id,serviceId:[16,30,31,34,42,46], statusLunch: false },
                 order: [
                     ['lastName', 'ASC'],
                 ],
@@ -72,7 +72,7 @@ class AlmuerzosService {
     static async getLunchEventuales(school_id) {
         try {
             const result = await Clients.findAll({
-                where: { sectionId: [5,6,7], schoolId: school_id, serviceId: 41, statusLunch: false },
+                where: { sectionId: [2,5,6,7], schoolId: school_id, serviceId: 48, statusLunch: false },
                 order: [
                     ['lastName', 'ASC'],
                 ],
