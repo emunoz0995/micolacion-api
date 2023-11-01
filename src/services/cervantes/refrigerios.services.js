a60465bc7ff30ba9da00b82bcea1cf8eb081e227
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-11-01 13:52:11 -0500

    upgrade

--- src/services/cervantes/refrigerios.services.js
+++ src/services/cervantes/refrigerios.services.js
@@ -72,7 +72,7 @@ class RefrigeriosService {
     static async getBreakFastEventuales(school_id) {
         try {
             const result = await Clients.findAll({
-                where: { sectionId: [2,5,6,7], schoolId: school_id, serviceId: 48, statusBreakfast: false },
+                where: { sectionId: [2,5,6,7], schoolId: school_id, serviceId: [48,30,31], statusBreakfast: false },
                 order: [
                     ['lastName', 'ASC'],
                 ],
