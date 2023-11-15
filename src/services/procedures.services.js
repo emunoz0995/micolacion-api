ad7cc8e5e3578cad786346fff0f0cb6dfdff73db
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-11-15 11:49:10 -0500

    corrigiendo condicional

--- src/services/procedures.services.js
+++ src/services/procedures.services.js
@@ -12,7 +12,11 @@ class RefrigeriosProcessService {
                 result.increment('breakfastConsumed', { by: 1 });
                 result.update({ statusBreakfast: true });
                 console.log(result.serviceId)
-                if (result.serviceId === (34 || 35 || 45 || 46 || 47)) {
+                if (result.serviceId === 34 || 
+                    result.serviceId === 35 || 
+                    result.serviceId === 45 || 
+                    result.serviceId === 46 || 
+                    result.serviceId === 47) {
                     result.decrement('totalLunch', { by: 1 });
                     result.increment('lunchesConsumed', { by: 1 });
                     result.update({ statusLunch: true })
@@ -68,7 +72,11 @@ class RefrigeriosProcessService {
                 result.increment('lunchesConsumed', { by: 1 });
                 result.update({ statusLunch: true });
                 console.log(result.serviceId)
-                if (result.serviceId === (34 || 35 || 45 || 46 || 47)) {
+                if (result.serviceId === 34 || 
+                    result.serviceId === 35 || 
+                    result.serviceId === 45 || 
+                    result.serviceId === 46 || 
+                    result.serviceId === 47) {
                     result.decrement('totalBreakfast', { by: 1 });
                     result.increment('breakfastConsumed', { by: 1 });
                     result.update({ statusBreakfast: true });
