73eae2fbe8b59c3a23970341b6b5cd59afeebc32
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-12-05 14:58:07 -0500

    reportes de excel

--- src/controllers/generateXML.js
+++ src/controllers/generateXML.js
@@ -38,8 +38,12 @@ const generateXML = async (req, res, next) => {
 
         const calculateTotal = () => {
             const subtotal = invoice.reduce((total, item) => total + item.quantity * item.price, 0);
-            const iva = Math.ceil((subtotal * 0.12).toFixed(2));
-            return (parseInt(subtotal) + parseFloat(iva)).toFixed(2);
+            const iva = (subtotal * 0.12).toFixed(2);
+            if (iva % 1 >= 0.05) {
+                return (parseInt(subtotal) + parseFloat(Math.ceil(iva))).toFixed(2);
+            } else {
+                return (parseInt(subtotal) + parseFloat(iva)).toFixed(2); 
+            }
         };
 
         const informacionFactura = {
