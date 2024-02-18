c5c82efba833eaf3c36e8d7022ab8f8d34e012be
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-02-18 13:24:17 -0500

    arreglo

--- src/services/reports.services.js
+++ src/services/reports.services.js
@@ -35,7 +35,7 @@ class ReportService {
                 where: { schoolId: school_id, 
                          sectionId:[1,2,3,4,5,6,7,9],
                          serviceId: {[Op.ne]: 48},
-                         [Op.or]:[
+                         [Op.and]:[
                            { totalBreakfast: { [Op.lte]: 5 }},
                            { totalLunch: { [Op.lte]: 5 }},        
                          ]
